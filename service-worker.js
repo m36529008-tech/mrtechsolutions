b59a9cef9c40bde92da5671809bafff8ee63e89f
Mahidhar Reddy <m36529008@gmail.com>
2026-01-01 03:05:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("mrtech-cache").then(cache =>
-      cache.addAll(["/","/index.html","/style.css","/script.js"])
+      cache.addAll([
+        "/mrtechsolutions/",
+        "/mrtechsolutions/index.html",
+        "/mrtechsolutions/style.css",
+        "/mrtechsolutions/script.js"
+      ])
     )
   );
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
